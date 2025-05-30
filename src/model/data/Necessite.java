b591c1618dcf7592c012fae3ef8a63b203e18469
Kern <Sterckx.keryann@gmail.com>
2025-05-30 19:09:01 +0200

    Add files via upload

---
+++ src/model/data/Necessite.java
@@ -0,0 +1,44 @@
+package model.data;
+
+public class Necessite {
+
+    private DPS dps;
+    private Competence competence;
+
+    // Constructeur 
+    public Necessite(DPS dps, Competence competence) {
+        setDps(dps);
+        setCompetence(competence);
+    }
+
+    // Getter DPS
+    public DPS getDps() {
+        return dps;
+    }
+
+    // Setter dps
+    public void setDps(DPS dps) {
+        if (dps == null) {
+            throw new IllegalArgumentException("DPS invalide");
+        }
+        this.dps = dps;
+    }
+
+    // Getter cpt
+    public Competence getCompetence() {
+        return competence;
+    }
+
+    // Setter cpt
+    public void setCompetence(Competence competence) {
+        if (competence == null) {
+            throw new IllegalArgumentException("Compétence invalide");
+        }
+        this.competence = competence;
+    }
+
+    @Override
+    public String toString() {
+        return "Le DPS n°" + dps.getId() + " nécessite la compétence : " + competence.getIntitule();
+    }
+}
