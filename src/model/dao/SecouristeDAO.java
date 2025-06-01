9d5090234f6dd6594ab586330c577fdc943e0108
Nolan Pécheul <nolanereac@gmail.com>
2025-06-01 18:19:34 +0000

    Update SecouristeDAO.java

--- src/model/dao/SecouristeDAO.java
+++ src/model/dao/SecouristeDAO.java
@@ -10,7 +10,7 @@ import model.data.Secouriste;
 
 public class SecouristeDAO extends DAO<Secouriste>{
     public int create(Secouriste secouriste) {
-        String query = "INSERT INTO Secouriste(ID , NOM, PRENOM, DATEDENAISSANCE, EMAIL, TEL, ADRESSE) VALUES ('" + secouriste.getId() + "','" 
+        String query = "INSERT INTO Secouriste(ID , NOM, PRENOM, DATENAISSANCE, EMAIL, TEL, ADRESSE) VALUES ('" + secouriste.getId() + "','" 
         + secouriste.getNom() + "','" + secouriste.getPrenom() + "','" + secouriste.getDateDeNaissance() + "','" + secouriste.getEmail() + "','" + secouriste.getTel() + "','" + secouriste.getAdresse() +"')";
         try (Connection con = getConnection (); 
         Statement st = con.createStatement ()) {
@@ -22,7 +22,7 @@ public class SecouristeDAO extends DAO<Secouriste>{
     }
 
     public int update(Secouriste secouriste) {
-        String query = "UPDATE SECOURISTE SET NOM='" + secouriste.getNom() + "', PRENOM='" + secouriste.getPrenom() +  "', DATEDENAISSANCE='" 
+        String query = "UPDATE Secouriste SET NOM='" + secouriste.getNom() + "', PRENOM='" + secouriste.getPrenom() +  "', DATENAISSANCE='" 
         + secouriste.getDateDeNaissance() +  "', EMAIL='" + secouriste.getEmail() +  "', TEL='" + secouriste.getTel() 
         +  "', ADRESSE='" + secouriste.getAdresse() + "' WHERE ID='" + secouriste.getId() + "'";
         try (Connection con = getConnection ();
@@ -35,7 +35,7 @@ public class SecouristeDAO extends DAO<Secouriste>{
     }
 
     public int delete(Secouriste secouriste) {
-        String query = "DELETE FROM SECOURISTE WHERE ID='" + secouriste.getId() + "'";
+        String query = "DELETE FROM Secouriste WHERE ID='" + secouriste.getId() + "'";
         try (Connection con = getConnection ();
         Statement st = con.createStatement ()) {
             return st.executeUpdate(query);
@@ -49,12 +49,12 @@ public class SecouristeDAO extends DAO<Secouriste>{
         List <Secouriste > secouristes = new LinkedList <>();
         try (Connection con = getConnection ();
         Statement st = con.createStatement ();
-        ResultSet rs = st.executeQuery("SELECT * FROM SECOURISTE")) {
+        ResultSet rs = st.executeQuery("SELECT * FROM Secouriste")) {
             while (rs.next()) {
                 long id = rs.getLong("ID");
                 String nom = rs.getString("NOM");
                 String prenom = rs.getString("PRENOM");
-                String dateDeNaissance = rs.getString("DATEDENAISSANCE");
+                String dateDeNaissance = rs.getString("DATENAISSANCE");
                 String email = rs.getString("EMAIL");
                 String tel = rs.getString("TEL");
                 String adresse = rs.getString("ADRESSE");
@@ -68,13 +68,13 @@ public class SecouristeDAO extends DAO<Secouriste>{
 
     public Secouriste findByID(long id) {
         try (Connection con = getConnection ();
-        PreparedStatement st = con.prepareStatement("SELECT * FROM SECOURISTE WHERE ID= ?")) {
+        PreparedStatement st = con.prepareStatement("SELECT * FROM Secouriste WHERE ID= ?")) {
             st.setLong(1, id);
             try(ResultSet rs = st.executeQuery ()){
                 if (rs.next()) {
                     String nom = rs.getString("NOM");
                     String prenom = rs.getString("PRENOM");
-                    String dateDeNaissance = rs.getString("DATEDENAISSANCE");
+                    String dateDeNaissance = rs.getString("DATENAISSANCE");
                     String email = rs.getString("EMAIL");
                     String tel = rs.getString("TEL");
                     String adresse = rs.getString("ADRESSE");
