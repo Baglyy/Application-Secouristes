f5290eeedb5074748123f880d66294644cfc7deb
Nolan Pécheul <nolanereac@gmail.com>
2025-06-04 10:35:48 +0200

    Update SecouristeDAO.java

--- src/model/dao/SecouristeDAO.java
+++ src/model/dao/SecouristeDAO.java
@@ -1,60 +1,81 @@
 package model.dao;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util .*;
+import java.sql.*;
+import java.util.*;
+
 import model.data.Secouriste;
 
-public class SecouristeDAO extends DAO<Secouriste>{
+public class SecouristeDAO extends DAO<Secouriste> {
 
     @Override
     public int create(Secouriste secouriste) {
-        String query = "INSERT INTO Secouriste(ID , NOM, PRENOM, DATENAISSANCE, EMAIL, TEL, ADRESSE) VALUES ('" + secouriste.getId() + "','" 
-        + secouriste.getNom() + "','" + secouriste.getPrenom() + "','" + secouriste.getDateDeNaissance() + "','" + secouriste.getEmail() + "','" + secouriste.getTel() + "','" + secouriste.getAdresse() +"')";
-        try (Connection con = getConnection (); 
-        Statement st = con.createStatement ()) {
-            return st.executeUpdate(query);
+        String query = "INSERT INTO Secouriste(ID, NOM, PRENOM, DATENAISSANCE, EMAIL, TEL, ADRESSE) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (Connection con = getConnection(); 
+             PreparedStatement pst = con.prepareStatement(query)) {
+
+            pst.setLong(1, secouriste.getId());
+            pst.setString(2, secouriste.getNom());
+            pst.setString(3, secouriste.getPrenom());
+            pst.setString(4, secouriste.getDateDeNaissance());
+            pst.setString(5, secouriste.getEmail());
+            pst.setString(6, secouriste.getTel());
+            pst.setString(7, secouriste.getAdresse());
+
+            return pst.executeUpdate();
+
         } catch (SQLException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
             return -1;
         }
     }
 
     @Override
     public int update(Secouriste secouriste) {
-        String query = "UPDATE Secouriste SET NOM='" + secouriste.getNom() + "', PRENOM='" + secouriste.getPrenom() +  "', DATENAISSANCE='" 
-        + secouriste.getDateDeNaissance() +  "', EMAIL='" + secouriste.getEmail() +  "', TEL='" + secouriste.getTel() 
-        +  "', ADRESSE='" + secouriste.getAdresse() + "' WHERE ID='" + secouriste.getId() + "'";
-        try (Connection con = getConnection ();
-        Statement st = con.createStatement ()) {
-            return st.executeUpdate(query);
+        String query = "UPDATE Secouriste SET NOM = ?, PRENOM = ?, DATENAISSANCE = ?, EMAIL = ?, TEL = ?, ADRESSE = ? WHERE ID = ?";
+        try (Connection con = getConnection();
+             PreparedStatement pst = con.prepareStatement(query)) {
+
+            pst.setString(1, secouriste.getNom());
+            pst.setString(2, secouriste.getPrenom());
+            pst.setString(3, secouriste.getDateDeNaissance());
+            pst.setString(4, secouriste.getEmail());
+            pst.setString(5, secouriste.getTel());
+            pst.setString(6, secouriste.getAdresse());
+            pst.setLong(7, secouriste.getId());
+
+            return pst.executeUpdate();
+
         } catch (SQLException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
             return -1;
         }
     }
 
     @Override
     public int delete(Secouriste secouriste) {
-        String query = "DELETE FROM Secouriste WHERE ID='" + secouriste.getId() + "'";
-        try (Connection con = getConnection ();
-        Statement st = con.createStatement ()) {
-            return st.executeUpdate(query);
+        String query = "DELETE FROM Secouriste WHERE ID = ?";
+        try (Connection con = getConnection();
+             PreparedStatement pst = con.prepareStatement(query)) {
+
+            pst.setLong(1, secouriste.getId());
+
+            return pst.executeUpdate();
+
         } catch (SQLException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
             return -1;
         }
     }
 
     @Override
-    public List <Secouriste > findAll () {
-        List <Secouriste > secouristes = new LinkedList <>();
-        try (Connection con = getConnection ();
-        Statement st = con.createStatement ();
-        ResultSet rs = st.executeQuery("SELECT * FROM Secouriste")) {
+    public List<Secouriste> findAll() {
+        List<Secouriste> secouristes = new LinkedList<>();
+        String query = "SELECT * FROM Secouriste";
+
+        try (Connection con = getConnection();
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
+
             while (rs.next()) {
                 long id = rs.getLong("ID");
                 String nom = rs.getString("NOM");
@@ -63,23 +84,27 @@ public class SecouristeDAO extends DAO<Secouriste>{
                 String email = rs.getString("EMAIL");
                 String tel = rs.getString("TEL");
                 String adresse = rs.getString("ADRESSE");
-                secouristes.add(new Secouriste(id , nom, prenom, dateDeNaissance, email, tel, adresse));
+
+                secouristes.add(new Secouriste(id, nom, prenom, dateDeNaissance, email, tel, adresse));
             }
+
         } catch (SQLException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
         }
+
         return secouristes;
     }
 
     @Override
     public Secouriste findByID(Object... keys) {
+        long id = (long) keys[0];
+        String query = "SELECT * FROM Secouriste WHERE ID = ?";
 
-        long id = (Long) keys[0];
-        
-        try (Connection con = getConnection ();
-        PreparedStatement st = con.prepareStatement("SELECT * FROM Secouriste WHERE ID= ?")) {
-            st.setLong(1, id);
-            try(ResultSet rs = st.executeQuery ()){
+        try (Connection con = getConnection();
+             PreparedStatement pst = con.prepareStatement(query)) {
+
+            pst.setLong(1, id);
+            try (ResultSet rs = pst.executeQuery()) {
                 if (rs.next()) {
                     String nom = rs.getString("NOM");
                     String prenom = rs.getString("PRENOM");
@@ -87,12 +112,15 @@ public class SecouristeDAO extends DAO<Secouriste>{
                     String email = rs.getString("EMAIL");
                     String tel = rs.getString("TEL");
                     String adresse = rs.getString("ADRESSE");
+
                     return new Secouriste(id, nom, prenom, dateDeNaissance, email, tel, adresse);
                 }
-            } catch (SQLException ex) { ex.printStackTrace (); }
-        } catch (SQLException ex) { 
-            ex.printStackTrace (); 
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
+
         return null;
     }
 }
