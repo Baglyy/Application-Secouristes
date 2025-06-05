32db3f6965afded83d56227f16a1e93826c124ca
SkɏNɨǥħŧ <luke.baffait@yahoo.com>
2025-06-05 11:17:53 +0200

    Add files via upload

--- src/model/AdminSecouristesModel.java
+++ src/model/AdminSecouristesModel.java
@@ -4,42 +4,32 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import model.dao.CompetenceDAO;
+import model.dao.SecouristeDAO;
+import model.data.Competence;
+import model.data.Secouriste;
+import java.util.ArrayList;
 
 public class AdminSecouristesModel {
     
     private final StringProperty nomUtilisateur = new SimpleStringProperty("");
     private final ObservableList<Secouriste> secouristes = FXCollections.observableArrayList();
-    
-    public AdminSecouristesModel() {
-        // Constructeur par défaut
-        initializeData();
-    }
+    private final ObservableList<Competence> competences = FXCollections.observableArrayList();
+    private final SecouristeDAO secouristeDAO;
+    private final CompetenceDAO competenceDAO;
     
     public AdminSecouristesModel(String nomUtilisateur) {
         this.nomUtilisateur.set(nomUtilisateur);
+        this.secouristeDAO = new SecouristeDAO();
+        this.competenceDAO = new CompetenceDAO();
         initializeData();
     }
-    
+
     private void initializeData() {
-        // Données d'exemple correspondant aux noms visibles dans l'image
-        secouristes.addAll(
-            // Première ligne
-            new Secouriste("Armand", "Barret", "Caron", "Delacroix", "Étienne"),
-            new Secouriste("Aubert", "Boucher", "Chantier", "Dubreuil", "Evrard"),
-            new Secouriste("Antoine", "Baudry", "Courtois", "Durand", "Eymard"),
-            new Secouriste("Allard", "Blanchet", "Chassagne", "Deschamps", "Escobar"),
-            new Secouriste("Augustin", "Brunet", "Colin", "Dufresne", "Eustache"),
-            
-            // Deuxième ligne  
-            new Secouriste("Fabre", "Gauthier", "Hubert", "Imbert", "Joubert"),
-            new Secouriste("Fontaine", "Grandin", "Hérault", "Isard", "Janvier"),
-            new Secouriste("Ferrand", "Garnier", "Huet", "Iribarren", "Jacquet"),
-            new Secouriste("Fleury", "Giraud", "Hémon", "Icard", "Jourdain"),
-            new Secouriste("Fournier", "Grimaud", "Houdin", "Isnard", "Jolivet")
-        );
+        secouristes.addAll(secouristeDAO.findAll());
+        competences.addAll(competenceDAO.findAll());
     }
     
-    // Getters pour les propriétés
     public StringProperty nomUtilisateurProperty() {
         return nomUtilisateur;
     }
@@ -48,66 +38,37 @@ public class AdminSecouristesModel {
         return secouristes;
     }
     
-    // Getters pour les valeurs
+    public ObservableList<Competence> getAllCompetences() {
+        return competences;
+    }
+    
     public String getNomUtilisateur() {
         return nomUtilisateur.get();
     }
     
-    // Setters
     public void setNomUtilisateur(String nomUtilisateur) {
         this.nomUtilisateur.set(nomUtilisateur);
     }
     
-    // Méthodes CRUD pour les secouristes
     public void ajouterSecouriste(Secouriste secouriste) {
-        secouristes.add(secouriste);
+        if (secouristeDAO.create(secouriste) > 0) {
+            secouristes.add(secouriste);
+        }
     }
     
     public void supprimerSecouriste(Secouriste secouriste) {
-        secouristes.remove(secouriste);
-    }
-    
-    public void modifierSecouriste(int index, Secouriste nouveauSecouriste) {
-        if (index >= 0 && index < secouristes.size()) {
-            secouristes.set(index, nouveauSecouriste);
+        if (secouristeDAO.delete(secouriste) > 0) {
+            secouristes.remove(secouriste);
         }
     }
     
-    // Classe interne pour représenter un secouriste
-    public static class Secouriste {
-        private final StringProperty nom1 = new SimpleStringProperty();
-        private final StringProperty nom2 = new SimpleStringProperty();
-        private final StringProperty nom3 = new SimpleStringProperty();
-        private final StringProperty nom4 = new SimpleStringProperty();
-        private final StringProperty nom5 = new SimpleStringProperty();
-        
-        public Secouriste(String nom1, String nom2, String nom3, String nom4, String nom5) {
-            this.nom1.set(nom1);
-            this.nom2.set(nom2);
-            this.nom3.set(nom3);
-            this.nom4.set(nom4);
-            this.nom5.set(nom5);
-        }
-        
-        // Getters pour les propriétés
-        public StringProperty nom1Property() { return nom1; }
-        public StringProperty nom2Property() { return nom2; }
-        public StringProperty nom3Property() { return nom3; }
-        public StringProperty nom4Property() { return nom4; }
-        public StringProperty nom5Property() { return nom5; }
-        
-        // Getters pour les valeurs
-        public String getNom1() { return nom1.get(); }
-        public String getNom2() { return nom2.get(); }
-        public String getNom3() { return nom3.get(); }
-        public String getNom4() { return nom4.get(); }
-        public String getNom5() { return nom5.get(); }
-        
-        // Setters
-        public void setNom1(String nom1) { this.nom1.set(nom1); }
-        public void setNom2(String nom2) { this.nom2.set(nom2); }
-        public void setNom3(String nom3) { this.nom3.set(nom3); }
-        public void setNom4(String nom4) { this.nom4.set(nom4); }
-        public void setNom5(String nom5) { this.nom5.set(nom5); }
+    public void updateSecouristeCompetences(Secouriste secouriste, ObservableList<Competence> newCompetences) {
+        secouriste.setCompetences(new ArrayList<>(newCompetences));
+        secouristeDAO.update(secouriste);
+    }
+    
+    public boolean secouristeExists(String nom, String prenom) {
+        return secouristeDAO.findByIdAndNom(0, nom) != null && 
+               secouristeDAO.findByIdAndNom(0, nom).getPrenom().equals(prenom);
     }
 }
