5974953b6b8627da041a4cf2c25ccc030fa8ba1c
Nolan Pécheul <nolanereac@gmail.com>
2025-06-05 10:40:44 +0200

    Update BDD.sql

--- SQL/BDD.sql
+++ SQL/BDD.sql
@@ -1,15 +1,3 @@
-/* Schéma relationnel : 
-Secouriste(id (1) (NN), nom (NN), prenom (NN), dateNaissance (NN), email (NN), tel (NN), adresse (NN))
-Competence(intitule (1))
-Possede([idSecouriste = @Secouriste.id, intituleComp = @Competence.intitule] (1))
-Journee([jour, mois, annee] (1))
-Site(code (1), nom (NN), longitude (NN), latitude (NN))
-Sport(code (1), nom (NN))
-DPS(id (1), horaire_depart (NN), horaire_fin (NN), codeSite = @Site.code (NN), codeSport = @Sport.code (NN), [jour = @Journee.jour, mois = @Journee.mois, annee = @Journee.annee] (NN))
-Besoin([intituleComp = @Competence.intitule, idDPS = @DPS.id] (1), nombre (NN))
-EstAffecteA([idSecouriste = @Secouriste.id, intituleComp = @Competence.intitule, idDPS = @DPS.id] (1))
-*/
-
 CREATE TABLE Secouriste (
     id BIGINT PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
@@ -22,14 +10,6 @@ CREATE TABLE Secouriste (
 
 CREATE TABLE Competence (
     intitule VARCHAR(50) PRIMARY KEY
-);
-
-CREATE TABLE Possede (
-    idSecouriste BIGINT,
-    intituleComp VARCHAR(50),
-    PRIMARY KEY (idSecouriste, intituleComp),
-    FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
-    FOREIGN KEY (intituleComp) REFERENCES Competence(intitule)
 );
 
 CREATE TABLE Journee (
@@ -53,30 +33,49 @@ CREATE TABLE Sport (
 
 CREATE TABLE DPS (
     id BIGINT PRIMARY KEY,
-    horaire_depart INT[2] NOT NULL,
-    horaire_fin INT[2] NOT NULL,
-    codeSite VARCHAR(50) NOT NULL,
-    codeSport VARCHAR(50) NOT NULL,
+    horaire_depart TIME NOT NULL,
+    horaire_fin TIME NOT NULL,
+    leSite VARCHAR(50) NOT NULL,
+    leSport VARCHAR(50) NOT NULL,
     jour INT NOT NULL,
     mois INT NOT NULL,
     annee INT NOT NULL,
-    FOREIGN KEY (codeSite) REFERENCES Site(code),
-    FOREIGN KEY (codeSport) REFERENCES Sport(code),
-    FOREIGN KEY (jour) REFERENCES Journee(jour),
-    FOREIGN KEY (mois) REFERENCES Journee(mois),
-    FOREIGN KEY (annee) REFERENCES Journee(annee)
+    FOREIGN KEY (leSite) REFERENCES Site(code),
+    FOREIGN KEY (leSport) REFERENCES Sport(code),
+    FOREIGN KEY (jour, mois, annee) REFERENCES Journee(jour, mois, annee)
 );
 
 CREATE TABLE Besoin (
     intituleComp VARCHAR(50),
     idDPS BIGINT,
     nombre INT NOT NULL,
-    PRIMARY KEY (intituleComp, idDPS)
+    PRIMARY KEY (intituleComp, idDPS),
+    FOREIGN KEY (intituleComp) REFERENCES Competence(intitule),
+    FOREIGN KEY (idDPS) REFERENCES DPS(id)
 );
 
-CREATE TABLE EstAffecteA (
-    idSecouriste BIGINT,
-    intituleComp VARCHAR(50),
-    idDPS BIGINT,
-    PRIMARY KEY (idSecouriste, intituleComp, idDPS)
+CREATE TABLE Possede (
+    idSecouriste BIGINT NOT NULL,
+    competence VARCHAR(255) NOT NULL,
+    PRIMARY KEY (idSecouriste, competence), -- Clé primaire composite
+    FOREIGN KEY (id_secouriste) REFERENCES Secouriste(id),
+    FOREIGN KEY (competence) REFERENCES Competence(intitule)
+);
+
+CREATE TABLE Disponibilite (
+    idSecouriste BIGINT NOT NULL,
+    jour INT NOT NULL,
+    mois INT NOT NULL,
+    annee INT NOT NULL,
+    PRIMARY KEY (idSecouriste, jour, mois, annee), -- Clé primaire composite
+    FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
+    FOREIGN KEY (jour, mois, annee) REFERENCES Journee(jour, mois, annee)
+);
+
+CREATE TABLE Affectation (
+    idSecouriste BIGINT NOT NULL,
+    idDps BIGINT NOT NULL,
+    PRIMARY KEY (idSecouriste, idDps), -- Clé primaire composite
+    FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
+    FOREIGN KEY (idDps) REFERENCES DPS(id)
 );
