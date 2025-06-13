a23ab1b1467b0922011315561ec7e5ddf1cc84a5
Nolan Pécheul <nolanereac@gmail.com>
2025-06-13 15:09:36 +0200

    Update BDD.sql

--- SQL/BDD.sql
+++ SQL/BDD.sql
@@ -6,30 +6,30 @@ CREATE TABLE Secouriste (
     email VARCHAR(50) NOT NULL,
     tel VARCHAR(50) NOT NULL,
     adresse VARCHAR(50) NOT NULL
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Competence (
     intitule VARCHAR(50) PRIMARY KEY
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Journee (
-    jour INT,
-    mois INT,
-    annee INT,
+    jour INT NOT NULL,
+    mois INT NOT NULL,
+    annee INT NOT NULL,
     PRIMARY KEY (jour, mois, annee)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Site (
     code VARCHAR(50) PRIMARY KEY,
     nom VARCHAR(50) NOT NULL,
     longitude FLOAT NOT NULL,
     latitude FLOAT NOT NULL
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Sport (
     code VARCHAR(50) PRIMARY KEY,
     nom VARCHAR(50) NOT NULL
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE DPS (
     id BIGINT PRIMARY KEY,
@@ -43,7 +43,7 @@ CREATE TABLE DPS (
     FOREIGN KEY (leSite) REFERENCES Site(code),
     FOREIGN KEY (leSport) REFERENCES Sport(code),
     FOREIGN KEY (jour, mois, annee) REFERENCES Journee(jour, mois, annee)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Besoin (
     intituleComp VARCHAR(50),
@@ -52,15 +52,15 @@ CREATE TABLE Besoin (
     PRIMARY KEY (intituleComp, idDPS),
     FOREIGN KEY (intituleComp) REFERENCES Competence(intitule),
     FOREIGN KEY (idDPS) REFERENCES DPS(id)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Possede (
     idSecouriste BIGINT NOT NULL,
-    competence VARCHAR(255) NOT NULL,
+    competence VARCHAR(50) NOT NULL,
     PRIMARY KEY (idSecouriste, competence), -- Clé primaire composite
     FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
     FOREIGN KEY (competence) REFERENCES Competence(intitule)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Disponibilite (
     idSecouriste BIGINT NOT NULL,
@@ -70,7 +70,7 @@ CREATE TABLE Disponibilite (
     PRIMARY KEY (idSecouriste, jour, mois, annee), -- Clé primaire composite
     FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
     FOREIGN KEY (jour, mois, annee) REFERENCES Journee(jour, mois, annee)
-);
+) ENGINE=InnoDB;
 
 CREATE TABLE Affectation (
     idSecouriste BIGINT NOT NULL,
@@ -78,4 +78,4 @@ CREATE TABLE Affectation (
     PRIMARY KEY (idSecouriste, idDps), -- Clé primaire composite
     FOREIGN KEY (idSecouriste) REFERENCES Secouriste(id),
     FOREIGN KEY (idDps) REFERENCES DPS(id)
-);
+) ENGINE=InnoDB;
