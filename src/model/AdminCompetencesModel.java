6149cfd5457e4fd7130ac1e28aae9bd76e140b58
SkɏNɨǥħŧ <luke.baffait@yahoo.com>
2025-06-18 15:11:36 +0200

    Add files via upload

--- src/model/AdminCompetencesModel.java
+++ src/model/AdminCompetencesModel.java
@@ -56,24 +56,6 @@ public class AdminCompetencesModel {
         }
     }
     
-    public void updateCompetence(Competence oldCompetence, Competence newCompetence) {
-        // Supprimer l'ancienne compétence
-        if (competenceDAO.delete(oldCompetence) > 0) {
-            competences.remove(oldCompetence);
-            // Créer la nouvelle compétence
-            if (competenceDAO.create(newCompetence) > 0) {
-                competences.add(newCompetence);
-            } else {
-                // En cas d'échec de la création, tenter de recréer l'ancienne compétence
-                competenceDAO.create(oldCompetence);
-                competences.add(oldCompetence);
-                throw new RuntimeException("Échec de la création de la nouvelle compétence.");
-            }
-        } else {
-            throw new RuntimeException("Échec de la suppression de l'ancienne compétence.");
-        }
-    }
-    
     public boolean competenceExists(String intitule) {
         return competenceDAO.findByID(intitule) != null;
     }
