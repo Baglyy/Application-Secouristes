94564cc80ce3dda8267f9c67a8e7dae05b90cacf
Kern <Sterckx.keryann@gmail.com>
2025-06-19 14:02:41 +0200

    Update DAO.java

--- src/model/dao/DAO.java
+++ src/model/dao/DAO.java
@@ -5,25 +5,80 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
-public abstract class DAO <T> {
+/**
+ * Classe abstraite générique représentant un DAO (Data Access Object).
+ * Fournit la structure de base pour les opérations CRUD et la gestion de la connexion JDBC.
+ *
+ * @param <T> le type d'entité manipulée par ce DAO
+ */
+public abstract class DAO<T> {
+
+    /** Nom de la classe du driver JDBC */
     private static String driverClassName = "com.mysql.cj.jdbc.Driver";
+    
+    /** URL de connexion à la base de données */
     private static String url = "jdbc:mysql://localhost/AppSecouristes";
+    
+    /** Nom d'utilisateur pour la connexion à la base */
     private static String username = "client";
+    
+    /** Mot de passe pour la connexion à la base */
     private static String password = "root";
-    protected Connection getConnection () throws SQLException {
+
+    /**
+     * Obtient une connexion JDBC à la base de données.
+     *
+     * @return la connexion JDBC
+     * @throws SQLException si une erreur survient pendant la connexion
+     */
+    protected Connection getConnection() throws SQLException {
         // Charger la classe du pilote
         try {
             Class.forName(driverClassName);
         } catch (ClassNotFoundException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
             return null;
         }
-        // Obtenir la connection
-        return DriverManager.getConnection(url , username , password);
+        // Obtenir la connexion
+        return DriverManager.getConnection(url, username, password);
     }
-    public abstract List <T> findAll ();
+
+    /**
+     * Récupère tous les éléments de la table.
+     *
+     * @return liste des objets de type T
+     */
+    public abstract List<T> findAll();
+
+    /**
+     * Recherche un élément par sa ou ses clé(s) primaire(s).
+     *
+     * @param keys une ou plusieurs clés pour identifier l’élément
+     * @return l’objet trouvé ou null
+     */
     public abstract T findByID(Object... keys);
+
+    /**
+     * Met à jour un élément existant dans la base.
+     *
+     * @param element l’élément à mettre à jour
+     * @return nombre de lignes affectées ou -1 en cas d’échec
+     */
     public abstract int update(T element);
+
+    /**
+     * Supprime un élément de la base.
+     *
+     * @param element l’élément à supprimer
+     * @return nombre de lignes affectées ou -1 en cas d’échec
+     */
     public abstract int delete(T element);
+
+    /**
+     * Crée un nouvel élément dans la base.
+     *
+     * @param element l’élément à insérer
+     * @return nombre de lignes insérées ou -1 en cas d’échec
+     */
     public abstract int create(T element);
 }
