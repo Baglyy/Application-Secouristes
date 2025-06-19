0ab11a1ae3aac57eac30f3235c81929af2cad51a
Kern <Sterckx.keryann@gmail.com>
2025-06-19 14:27:16 +0200

    Update SiteDAO.java

--- src/model/dao/SiteDAO.java
+++ src/model/dao/SiteDAO.java
@@ -5,13 +5,22 @@ import java.util.*;
 
 import model.data.Site;
 
+/**
+ * DAO (Data Access Object) pour gérer les opérations CRUD sur les sites.
+ */
 public class SiteDAO extends DAO<Site> {
 
+    /**
+     * Insère un nouveau site dans la base de données.
+     * 
+     * @param site le site à insérer
+     * @return le nombre de lignes affectées, ou -1 en cas d'erreur
+     */
     @Override
     public int create(Site site) {
         String query = "INSERT INTO Site(CODE, NOM, LONGITUDE, LATITUDE) VALUES (?, ?, ?, ?)";
         try (Connection con = getConnection(); 
-            PreparedStatement pst = con.prepareStatement(query)) {
+             PreparedStatement pst = con.prepareStatement(query)) {
 
             pst.setString(1, site.getCode());
             pst.setString(2, site.getNom());
@@ -26,11 +35,17 @@ public class SiteDAO extends DAO<Site> {
         }
     }
 
+    /**
+     * Met à jour les informations d'un site existant.
+     * 
+     * @param site le site à mettre à jour
+     * @return le nombre de lignes affectées, ou -1 en cas d'erreur
+     */
     @Override
     public int update(Site site) {
         String query = "UPDATE Site SET NOM = ?, LONGITUDE = ?, LATITUDE = ? WHERE CODE = ?";
-        try (Connection con = getConnection ();
-            PreparedStatement pst = con.prepareStatement (query)) {
+        try (Connection con = getConnection();
+             PreparedStatement pst = con.prepareStatement(query)) {
             
             pst.setString(1, site.getNom());
             pst.setFloat(2, site.getLongitude());
@@ -39,16 +54,22 @@ public class SiteDAO extends DAO<Site> {
 
             return pst.executeUpdate();
         } catch (SQLException ex) {
-            ex.printStackTrace ();
+            ex.printStackTrace();
             return -1;
         } 
     }
 
+    /**
+     * Supprime un site de la base de données.
+     * 
+     * @param site le site à supprimer
+     * @return le nombre de lignes supprimées, ou -1 en cas d'erreur
+     */
     @Override
     public int delete(Site site) {
         String query = "DELETE FROM Site WHERE CODE = ?";
         try (Connection con = getConnection(); 
-            PreparedStatement pst = con.prepareStatement(query)) {
+             PreparedStatement pst = con.prepareStatement(query)) {
 
             pst.setString(1, site.getCode());
             return pst.executeUpdate();
@@ -59,14 +80,19 @@ public class SiteDAO extends DAO<Site> {
         }
     }
 
+    /**
+     * Récupère la liste de tous les sites présents en base.
+     * 
+     * @return une liste de sites
+     */
     @Override
     public List<Site> findAll() {
         List<Site> sites = new LinkedList<>();
         String query = "SELECT * FROM Site";
 
         try (Connection con = getConnection();
-            Statement st = con.createStatement();
-            ResultSet rs = st.executeQuery(query)) {
+             Statement st = con.createStatement();
+             ResultSet rs = st.executeQuery(query)) {
 
             while (rs.next()) {
                 String code = rs.getString("CODE");
@@ -83,14 +109,19 @@ public class SiteDAO extends DAO<Site> {
         return sites;
     }
 
+    /**
+     * Recherche un site à partir de son code.
+     * 
+     * @param keys les clés d'identification, ici un seul String représentant le code
+     * @return le site trouvé ou null si aucun résultat
+     */
     @Override
     public Site findByID(Object... keys) {
-
         String codeString = (String) keys[0];
         
         String query = "SELECT * FROM Site WHERE CODE = ?";
         try (Connection con = getConnection();
-            PreparedStatement pst = con.prepareStatement(query)) {
+             PreparedStatement pst = con.prepareStatement(query)) {
 
             pst.setString(1, codeString);
             try (ResultSet rs = pst.executeQuery()) {
