2d0aa524dca67357a1a0cfc3faff6fab2752f52e
Kern <Sterckx.keryann@gmail.com>
2025-06-19 14:29:59 +0200

    Update TestSecouristeDAO.java

--- src/model/dao/TestSecouristeDAO.java
+++ src/model/dao/TestSecouristeDAO.java
@@ -3,7 +3,25 @@ package model.dao;
 import model.dao.SecouristeDAO;
 import model.data.Secouriste;
 
+/**
+ * Classe de test pour le DAO {@link SecouristeDAO}.
+ * Ce test manuel permet de vérifier le bon fonctionnement des opérations de base
+ * sur l'entité {@link Secouriste}, à savoir : création, lecture, mise à jour et suppression (CRUD).
+ */
 public class TestSecouristeDAO {
+
+    /**
+     * Point d'entrée principal pour le test du DAO Secouriste.
+     * <p>Ce test couvre les cas suivants :</p>
+     * <ul>
+     *     <li>Création d'un secouriste</li>
+     *     <li>Recherche d'un secouriste par ID</li>
+     *     <li>Mise à jour des informations du secouriste</li>
+     *     <li>Suppression du secouriste</li>
+     * </ul>
+     *
+     * @param args les arguments de la ligne de commande (non utilisés ici)
+     */
     public static void main(String[] args) {
         SecouristeDAO dao = new SecouristeDAO();
 
